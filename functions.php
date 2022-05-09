0cf65746327be8e21ccf252cec903d9ac498aebc
andredw13L <andreribeiro13l@gmail.com>
2022-05-08 23:57:47 -0300

    Admin - Pedidos

--- functions.php
+++ functions.php
@@ -13,11 +13,18 @@ function get($key)
 	return str_replace("'", "", $_GET[$key]);
 }
 
-function formatPrice( $vlprice)
+function formatPrice($vlprice)
 {
 	if(!$vlprice>0 ) $vlprice = 0;
 
 	return number_format($vlprice, 2, ",", ".");
+}
+
+function formatDate($date)
+{
+
+	return date('d/m/Y', strtotime($date));
+
 }
 
 function checkLogin($inadmin = true)
--- views-cache/header.1b946dd8a1c9a1d4acbcc4b186d3f307.rtpl.php
+++ views-cache/header.1b946dd8a1c9a1d4acbcc4b186d3f307.rtpl.php
@@ -58,7 +58,7 @@ desired effect
   <header class="main-header">
 
     <!-- Logo -->
-    <a href="/res/admin/index2.html" class="logo">
+    <a href="index2.html" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>A</b>LT</span>
       <!-- logo for regular state and mobile devices -->
@@ -257,20 +257,11 @@ desired effect
         <li class="header">HEADER</li>
         <!-- Optionally, you can add icons to the links -->
         <li><a href="/admin/users"><i class="fa fa-users"></i> <span>Usuários</span></a></li>
-        <li><a href="/admin/categories"><i class="fa fa-filter"></i> <span>Categorias</span></a></li>
+        <li><a href="/admin/categories"><i class="fa fa-link"></i> <span>Categorias</span></a></li>
         <li><a href="/admin/products"><i class="fa fa-link"></i> <span>Produtos</span></a></li>
+        <li><a href="/admin/orders"><i class="fa fa-shopping-cart"></i> <span>Pedidos</span></a></li>
 
-        <li class="treeview">
-          <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="#">Link in level 2</a></li>
-            <li><a href="#">Link in level 2</a></li>
-          </ul>
-        </li>
+        
       </ul>
       <!-- /.sidebar-menu -->
     </section>
