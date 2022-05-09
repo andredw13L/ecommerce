5ae7f9ed72eabbe9ae67d94298ebbd74f516e000
andredw13L <andreribeiro13l@gmail.com>
2022-05-09 00:57:35 -0300

    Admin - Pedidos - Busca e Paginação

--- index.php
+++ index.php
@@ -452,8 +452,6 @@ $app->get("/admin/products", function(){
 
 	}
 
-	$products = Product::listAll();
-
 	$page = new PageAdmin();
 
 	$page->setTpl("products", [
@@ -1303,15 +1301,43 @@ $app->get("/admin/orders", function(){
 
 	User::verifyLogin();
 
+	$search = (isset($_GET['search'])) ? $_GET['search'] : "";
+	$page = (isset($_GET['page'])) ? (int)$_GET['page'] : 1;
+
+	if ($search != '') {
+
+		$pagination = Order::getPageSearch($search, $page);
+
+	} else {
+
+		$pagination = Order::getPage($page);
+
+	}
+
+	$pages = [];
+
+	for ($x = 0; $x < $pagination['pages']; $x++)
+	{
+
+		array_push($pages, [
+			'href'=>'/admin/orders?'.http_build_query([
+				'page'=>$x+1,
+				'search'=>$search
+			]),
+			'text'=>$x+1
+		]);
+
+	}
 
 	$page = new PageAdmin();
 
 	$page->setTpl("orders", [
-		"orders"=>Order::listAll()
+		"orders"=>$pagination['data'],
+		"search"=>$search,
+		"pages"=>$pages
 	]);
 
 });
-
 
 
 $app->run();
